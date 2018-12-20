335214bde3f411e56d4c74cac2c9ae9e0c3db73b
Stella-Ndirangu <stellaw.ndirangu@gmail.com>
2018-12-20 12:14:11 +0300

    Changes to feedbackform

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -1.277335, lng: 36.818145},
-    zoom: 8
-  });
-  var contentString = '<div id="content">'+
-              '<div id="siteNotice">'+
-              '</div>'+
-              '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-              '<div id="bodyContent">'+
-              '<p><b>EXECUTIVE Printing Works ltd(EPW) - Mission:</b><br>Our Mission is to provide high quality print services ' +
-              'services to our clients with timely delivery and customer satisfaction. '+
-              'The passion we have for our work enables us to undertake of our clients '+
-              'jobs. We constantly strive to deliver the highest-quality products possible. '+
-              'There is no greater satisfaction than delivering a finished product '+
-              'that helps a client reach their goals and objectives. '+
-              '<p>EPW, <a href="https://http://executive.office-on-the.net/">'+
-              '</div>'+
-              '</div>';
-  var marker = new google.maps.Marker({
-    position:{lat: -1.277335, lng: 36.818145},
-    map:map,
-    icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-    // icon:'accounting.svg'
-  })
-  var locationInfo = new google.maps.InfoWindow({
-    content: contentString
-  });
-  marker.addListener('click', function(){
-    locationInfo.open(map, this);
-  })
-}
-=======
-
 $(document).ready(function(){
   var form = $("#requestQuotation");
   $("#requestQuotation").submit(function(event){
@@ -74,13 +38,12 @@ $(document).ready(function(){
   //End of submit form function
 //Fridah///////..............................................................
 // Question one
-$(document).ready(function() {
     $("#start-btn").click(function(){
     event.preventDefault();
     var userName = $("#customerName").val();
     var userEmail = $("#customerEmail").val();
     var userNumber= $("#customerNumber").val();
-   
+
     if(userName.length === 0 || userEmail.length === 0 || userNumber.length ==0){
       $("#no-details").show();}
       else{
@@ -89,9 +52,9 @@ $(document).ready(function() {
     }
 
 
-    $("form#ratingForm").submit(function(e) 
+    $("form#ratingForm").submit(function(e)
     {
-        e.preventDefault(); 
+        e.preventDefault();
         if ($("#ratingForm :radio:checked").length == 0) {
             $('#status').html("nothing checked");
             return false;
@@ -115,14 +78,14 @@ $(document).ready(function() {
         $(".thumbsdown").show();
         $(".suggest").show();
     });
- 
+
   //question 3 and 4
-  
+
   var feedbacks = ["recommendation","redo"];
   feedbacks.forEach(function(feedback) {
     var userValue = $("input:radio[name=" + feedback + "]:checked").val();
   });
-  
+
   $("#finished").click(function(){
       $(".questions").hide();
     $(".thankyou").show();
@@ -130,4 +93,3 @@ $(document).ready(function() {
   });
 
 });
->>>>>>> f57a17d5defdf9b4005aec6e9b122b1b363e98a0
